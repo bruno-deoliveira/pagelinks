842d07f90046fcd5fedb259b27af1dc6a16688af
Bruno <brunoalmeida0805@gmail.com>
2023-10-07 22:26:42 -0300

    add toggle mode function

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const body = document.body
+  body.classList.toggle("light")
+
+  //ou pode fazer assim
+  /*if(body.classList.contains('light')){
+    body.classList.remove('light')
+  } else {
+    body.classList.add("light")
+  }*/
+
+  //pegar a tag imagem
+  const img = document.querySelector("#profile img")
+  //substituir a imagem do perfil
+  if (body.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+  // set tiver sem light mode, manter a imagem normal  
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
